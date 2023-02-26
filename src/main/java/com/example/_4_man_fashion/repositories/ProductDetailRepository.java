9648952313a5db5913184a0cab3212b78ebfb36c
hoanam2k2 <hoanam2k2.tb@gmail.com>
2023-02-26 10:16:31 +0700

    api CRUD product

--- src/main/java/com/example/_4_man_fashion/repositories/ProductDetailRepository.java
+++ src/main/java/com/example/_4_man_fashion/repositories/ProductDetailRepository.java
@@ -2,6 +2,7 @@ package com.example._4_man_fashion.repositories;
 
 import com.example._4_man_fashion.entities.ProductDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,10 +12,8 @@ import java.util.List;
 @Repository
 public interface ProductDetailRepository extends JpaRepository<ProductDetail, Integer> {
 
-//    @Query("select p from ProductDetail p where p.productId = :product_id and p.status =:status")
-//    List<ProductDetail> getProductDetailsByProductId(
-//            @Param("product_id") Integer product_id ,
-//            @Param("status") Integer status
-//    );
+    @Modifying
+    @Query("update ProductDetail p set p.status = :status where p.product.productId = :product_id and ((p.status = 1 and :status = 0) or (p.status = 0 and :status = 1) ) ")
+    void updateProductDetailStatus(@Param("product_id") Integer product_id, @Param("status") Integer status); // Chỉ cập nhật sản phẩm chi tiết có trạng thái 0 và 1
 
 }
