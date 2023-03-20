8a806cbee2d3db064731bcd2a4413931a4cac27d
LeVanTrangZ <levantrang4302@gmail.com>
2023-03-20 22:40:45 +0700

    add table log order status

---
+++ src/main/resources/db/migration/V1.0022__create_table_log_order_status.sql
@@ -0,0 +1,11 @@
+CREATE TABLE LOG_ORDER_STATUS
+(
+    id serial primary key,
+    order_id bigint not null ,
+    times timestamp DEFAULT current_timestamp,
+    user_change varchar(255),
+    note text,
+    new_status int default 1,
+    account_id bigint not null,
+    constraint fk_accounts FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
