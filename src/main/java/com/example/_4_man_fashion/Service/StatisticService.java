c749e14be1e14ea42265941932155efa5a9876a8
Phus <vumanhphu05122002@gmail.com>
2023-04-19 23:43:31 +0700

    update api statistic

---
+++ src/main/java/com/example/_4_man_fashion/Service/StatisticService.java
@@ -0,0 +1,21 @@
+package com.example._4_man_fashion.Service;
+
+import com.example._4_man_fashion.dto.StatisticFavorite;
+import com.example._4_man_fashion.dto.StatisticIncome;
+import com.example._4_man_fashion.dto.StatisticRevenue;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+
+public interface StatisticService {
+
+    List<StatisticFavorite> statisticsByBestFavoriteProducts(Date time1, Date time2);
+    List<StatisticIncome> getStatisticByDate(Date time1, Date time2);
+    List<StatisticIncome> getStatisticIncomeByYear(Integer year);
+    List<Integer> statisticOrderStatus(Date s_date, Date e_date);
+    List<StatisticFavorite> statisticsByBestSellingProducts(Date time1, Date time2);
+    int getAmountOrder(Date time1, Date time2);
+    List<StatisticRevenue> getRevenue(Date time1, Date time2);
+    StatisticRevenue getRevenueAndQuantityOrder(Date time1, Date time2) throws ParseException;
+}
